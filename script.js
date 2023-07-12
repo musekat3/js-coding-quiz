c287bcb962d20ebe3286b71e2c1a7cefb08c6f8a
Kathy Hyde <musekat3@gmail.com>
2023-07-11 20:29:53 -0700

    Updates

--- script.js
+++ script.js
@@ -15,23 +15,45 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 }
 var myQuestions = [
 	{
-		question: "What is 10/2?",
+		question: "Which on of these is NOT a way to use CSS in a webpage?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
-		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "What is 30/3?",
-		answers: {
-			a: '3',
-			b: '5',
-			c: '10'
+			a: 'inline',
+			b: 'external',
+			c: 'outline',
+			d: 'internal',
 		},
 		correctAnswer: 'c'
-	}
+	},
+	{
+		question: "Which element contains information that helps the browser to render the page correctly?",
+		answers: {
+			a: '<h>',
+			b: '<title>',
+			c: '<p>',
+			d: '<link>',
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "What does DOM stand for?",
+		answers: {
+			a: 'Download Object Model',
+			b: 'Document Obtuse Media',
+			c: 'Document Object Model',
+			d: 'Download object Media',
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "What is the purpose of APIs?",
+		answers: {
+			a: 'to connect user to their respositories',
+			b: 'to interact with components of a web browser',
+			c: 'to handle user interaction',
+			d: 'to help define functions',
+		}, 
+		correctAnswer: 'b'
+	},
 ];
 function showQuestions(questions, quizContainer){
 	var output = [];
@@ -97,3 +119,26 @@ var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
 
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+function countdown() {
+  var timeLeft = 60;
+
+  var timeInterval = setInterval(function () {
+
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+countdown();
